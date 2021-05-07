98cddd5d2793e08fa275c412d38087596551a15e
Jacob Yeung <jacobyeung@berkeley.edu>
2021-05-07 08:04:07 -0700

    utils contains rudimentary select_filters

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -1,6 +1,11 @@
 import os
+import torch
+import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-
+import tensorly as tl
+import tensorly.decomposition as dc
+from tqdm import tqdm
+tl.set_backend('pytorch')
 
 def create_summary_writer(model, data_loader, save_folder, model_id, device='cpu', conv=False):
     """Create a logger.
@@ -34,3 +39,33 @@ def create_summary_writer(model, data_loader, save_folder, model_id, device='cpu
         except Exception as e:
             print("Failed to save model graph: {}".format(e))
     return writer
+
+def select_filters(model, valid_loader, valid_set, device):
+    """
+    worst : list of highest divergence filters (worst filters) across batches
+            Can select top-k afterwards.
+    imp   : list of divergences from tensor decomposition reconstruction.
+            lower means filter is more important.
+    """
+    imp = []
+    worst = []
+    for i, data in tqdm(enumerate(valid_loader),
+                        total=len(valid_set) / valid_loader.batch_size):
+        x, y = data
+        x = x.to(device)
+        y = y
+        out = model(x)
+        nout = out.detach()
+        ny = y.detach().numpy()
+        for j in range(out.shape[-1]//10, out.shape[-1], out.shape[-1]//4):
+            print(j)
+            cp = dc.tucker(nout, j)
+            pred = tl.tucker_tensor.tucker_to_tensor(cp)
+            dist = torch.cdist(pred, nout)
+            importance = torch.mean(dist, dim=[0, 2, 3])
+            w = torch.argmax(importance)
+            worst.append(w)
+            imp.append(importance.clone().detach())
+        if i == 10:
+            break
+    return worst, imp
